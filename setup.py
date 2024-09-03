5652c657480f2eb17491125576e2ed9c3198ad6b
saisriharsha19 <149857181+saisriharsha19@users.noreply.github.com>
2024-09-03 21:51:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
 	name='assignment0',
 	version='1.0',
-	author='You Name',
-	author_email='your UFL email',
+	author='Sai Sri Harsha Guddati',
+	author_email='saisriha.guddati@ufl.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']	
